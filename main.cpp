d20b3b7cb7284c306bc06cf4ca8abeb498a43f47
Karina Lopez <karinalopez@karinas-mbp.lan>
2021-02-24 20:01:26 -0500

    instantiate book class

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 #include "book.hpp"
 
 int main() {
+    Book magicalRealism;
+    magicalRealism.setAuthor("Isabel Allende");
+    magicalRealism.setGenre("Magical Realism");
+    magicalRealism.setTitle("Daughter of Fortune");
+
+    std::cout<< magicalRealism.getAuthor()<< " is a "<< magicalRealism.getGenre()<<" writer. One of the author's books from this genre is "<<magicalRealism.getTitle()<<"/n";
 
 }
